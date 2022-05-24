26d1a9b9b879fd5a9ae107587c662f1bd4de0ebf
pavithramuddu93 <pavithramuddu93>
2022-05-24 09:11:20 +0530

    UC2 added

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -2,12 +2,12 @@ import com.blz.MoodAnalyzer;
 import org.junit.jupiter.api.Test;
 
 import static junit.framework.Assert.assertEquals;
+import junit.framework.Assert;
 public class MoodAnalyzerTest {
-
     MoodAnalyzer moodAnalyzer;
 
     @Test
-    public void givenSad_shouldReturnSad(){
+    public void givenSad_shouldReturnSad() {
         moodAnalyzer = new MoodAnalyzer("SAD");
         String result = moodAnalyzer.analyseMood();
         assertEquals("SAD", result);
@@ -15,10 +15,22 @@ public class MoodAnalyzerTest {
     }
 
     @Test
-    public void givenAny_shouldReturnHappy(){
+    public void givenAny_shouldReturnHappy() {
         moodAnalyzer = new MoodAnalyzer("any");
         String result = moodAnalyzer.analyseMood();
         assertEquals("HAPPY", result);
 
+    }
+
+    @Test
+    public void givenNull_shouldReturnHappy() {
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer(null);
+        String result = null;
+        try {
+            result = moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY", result);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 }

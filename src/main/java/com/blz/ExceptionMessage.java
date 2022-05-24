cc983e448a5f6eecaca02b55e1f2444873b3be3c
pavithramuddu93 <pavithramuddu93>
2022-05-24 09:17:39 +0530

    UC3 added

---
+++ src/main/java/com/blz/ExceptionMessage.java
@@ -0,0 +1,21 @@
+package com.blz;
+
+
+public enum ExceptionMessage implements CharSequence {
+    Empty;
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return 0;
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return null;
+    }
+}
